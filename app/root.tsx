652fea998f5ef682b3d7bcb0a855dbfa1dc83d7d
Chii Yuen <chii.yuen@hotmail.com>
2026-03-23 14:20:11 +0800

    Add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,18 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon-light.svg",
+    media: "(prefers-color-scheme: light)",
+  },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon-dark.svg",
+    media: "(prefers-color-scheme: dark)",
+  },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
